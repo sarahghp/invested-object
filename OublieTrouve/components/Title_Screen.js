efd940b7d5c493dfa779681e96bfbbc76cd672f0
Sarah GP <sarah.groff.palermo@gmail.com>
2016-07-12 11:36:00 -0700

    Swap backsplash logo for image to make lighthouse appear everyhwere

--- OublieTrouve/components/Title_Screen.js
+++ OublieTrouve/components/Title_Screen.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View, 
+  Image
 } from 'react-native';
 
 import shorthand from 'react-native-styles-shorthand';
@@ -13,45 +14,15 @@ export default class Title extends Component {
   render() {
     return (
       <View style={styles.titleContainer}>
-        <Text style={styles.title}>
-          <Text style={styles.O}>
-            O/
-          <Text style={styles.t}>
-            t
-          </Text></Text>
-        </Text>
-        <View style={styles.bgLight}>
-          <Lighthouse style={{size: '96', stroke: '#163939', strWidth: '0.5'}}/>
-        </View>
+        <Image source={require('./img/splash-logo.png')} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create(shorthand({
-  bgLight: {
-    position: 'absolute',
-    top: 200,
-    left: 38,
-  },
   titleContainer: {
     flex: 6,
     justifyContent: 'center',
   },
-  title: {
-    color: '#fff',
-    margin: '0 0 0 -20',
-    letterSpacing: -35,
-  },
-  O: {
-    fontSize: 240,
-    fontFamily: 'Futura',
-    textAlign: 'center',
-  },
-  t: {
-    fontSize: 154,
-    fontFamily: 'PT Serif',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
 }));
